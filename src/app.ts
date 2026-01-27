138f55f473198619ef373d5c36e70161d2cfdbf7
Nyasha Karata <veximagames@gmail.com>
2026-01-27 14:12:53 +0200

    s

--- src/app.ts
+++ src/app.ts
@@ -38,6 +38,7 @@ app.use(cors({
     "http://31.220.82.129:3003",
     "http://172.20.20.10.9:5001",
     "https://car-parts-hub.vercel.app",
+    "https://simbi-market-sigma.vercel.app",
     "https://simbi-admin.vercel.app",
     "https://simbi-seller.vercel.app",
     "https://carspain.vercel.app",
