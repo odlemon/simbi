3ead824e5e8d2c74a552f48b79aeb83d272d2506
Nyasha Karata <veximagames@gmail.com>
2026-05-26 08:51:48 +0200

    save

--- src/controllers/seller/notifications/SellerNotificationController.ts
+++ src/controllers/seller/notifications/SellerNotificationController.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { Response } from 'express';
 import { AuthenticatedRequest } from '../../../types';
 import { SellerNotificationService } from '../../../services/seller/notifications/SellerNotificationService';
